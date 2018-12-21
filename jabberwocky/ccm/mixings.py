525d313bb5fe76415930f533dc189f0abc48b1b1
Kyle Wroble <Kyle.Wroble@smithgroup.com>
2018-12-21 09:12:28 -0500

    Diff report from pyaxl to jabberwocky - mixings.py
    15c4e71 2018-10-05 16:55:33 -0400:
    Reorganized folder structure to resemble python package.
    
    a0f8ce9 2018-10-19 08:12:45 -0400:
    Implemented the set_associated_devices function for User
    
    e90c744 2018-10-24 09:15:31 -0400:
    Implemented line functions for MixingAbstractLines
    
    5676c1b 2018-12-17 08:56:24 -0500:
    Wrote function to associate an end user with a line appearance
    
    b331e14 2018-12-17 15:59:42 -0500:
    Modified set_line_user_association to handle userid passed as a string

--- jabberwocky/ccm/mixings.py
+++ jabberwocky/ccm/mixings.py
@@ -1,6 +1,6 @@
 import logging
 import types
-
+from jabberwocky.ccm.xtypes import XPhoneLine, XEnduserMember
 
 log = logging.getLogger('pyaxl')
 
@@ -19,29 +19,45 @@ class MixingAbstractTemplate(object):
         log.debug('%s created from template, criteria: %s, %s' % (cls.__name__, str(args), str(kwargs),))
         obj = template.clone()
         if typeclass is not None:
-            obj.cls = typeclass
+            obj['class'] = typeclass
         return obj
 
 
 class MixingAbstractLines(object):
 
     def set_lines(self, lines):
-        """ associate a list or a single ccm.Line object
-            to deviceprofile.
-        """
+        """Associate a list or a single ccm.Line object to device."""
+
         if not isinstance(lines, (list, types.GeneratorType)):
             lines = [lines]
-        self.lines = list()
-        for index, line in enumerate(lines):
-            self.lines.append(dict(line=dict(index=index + 1, dirn=dict(_uuid=line._uuid))))
+        self.set_phone_lines(self._line_to_phone_line(lines))
 
-    def set_phonelines(self, xphonelines):
-        """ associate a list or a single ccm.XPhoneLine object
-            to deviceprofile.
-        """
-        if not isinstance(xphonelines, list):
-            xphonelines = [xphonelines]
-        self.lines = list()
-        for index, xpl in enumerate(xphonelines):
-            xpl.index = index + 1
-            self.lines.append(dict(line=xpl))
+    def set_line_user_association(self, user, phone_line):
+        """Associate an end user with a line appearance."""
+        if not phone_line.associatedEndusers:
+            phone_line.associatedEndusers = {'enduser': []}
+        if isinstance(user, str):
+            enduser = XEnduserMember(userId=user)
+        else:
+            enduser = XEnduserMember(userId=user.userid)
+        phone_line.associatedEndusers['enduser'].append(enduser)
+        self.lines = self.lines
+
+    def set_phone_lines(self, phone_lines):
+        """Associate a list or a single ccm.XPhoneLine object to device."""
+        if not isinstance(phone_lines, (list, types.GeneratorType)):
+            phone_lines = [phone_lines]
+        self.lines = {'line': phone_lines}
+
+    def remove_phone_lines(self, phone_lines):
+        """Disassociate a list or a single ccm.XPhoneLine object from device."""
+        if not isinstance(phone_lines, (list, types.GeneratorType)):
+            phone_lines = [phone_lines]
+        self.removeLines = {'line': phone_lines}
+
+    def _line_to_phone_line(self, lines):
+        """Convert a list or single ccm.Line object to XPhoneLine object."""
+        if not isinstance(lines, (list, types.GeneratorType)):
+            lines = [lines]
+        for index, line in enumerate(lines):
+            yield XPhoneLine(index=index + 1, dirn={'pattern': line.pattern, 'uuid': line.uuid})
