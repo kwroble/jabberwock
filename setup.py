b3d89e6a1a0c61a56cdb07413aa194112ff471ad
kwroble <kyle.wroble@smithgroup.com>
2021-11-23 14:24:18 -0500

    Configuring GitHub actions to lint, package, and deploy to Test PyPI

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jabberwocky",
+    version="0.5.0",
+    author="Kyle Wroble",
+    author_email="kwroble@gmail.com",
+    url="https://github.com/kwroble",
+    description="Python library that helps with accessing Cisco Call Manager over the AXL interface",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["appdirs", "attrs", "boltons", "cached-property", "certifi", "chardet", "defusedxml", "idna",
+                      "isodate", "lxml", "pytz", "requests", "requests-toolbelt", "six", "urllib3", "zeep"],
+)
