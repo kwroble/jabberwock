3342f1850c54aa9d2ef039ef8dbd3fe4a7f20ea7
kwroble <kyle.wroble@smithgroup.com>
2021-11-24 13:58:46 -0500

    Fixing PyPI token

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jabberwock",
-    version="0.6.1",
+    version="0.6.2",
     author="Kyle Wroble",
     author_email="kwroble@gmail.com",
     url="https://github.com/kwroble",
