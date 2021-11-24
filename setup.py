d441a9671ac0459b9ae5c540192d3449284b4c69
kwroble <kyle.wroble@smithgroup.com>
2021-11-24 13:51:09 -0500

    Incrementing version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jabberwock",
-    version="0.6.0",
+    version="0.6.1",
     author="Kyle Wroble",
     author_email="kwroble@gmail.com",
     url="https://github.com/kwroble",
