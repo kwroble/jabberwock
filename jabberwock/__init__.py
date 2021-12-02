10d88abe6682f86b15a8aa06d353601d4d4f2d61
kwroble <kyle.wroble@smithgroup.com>
2021-12-02 14:40:49 -0500

    Moved _version.py to base directory #patch

--- jabberwock/__init__.py
+++ jabberwock/__init__.py
@@ -1,3 +1,2 @@
 from jabberwock.configuration import registry
 from jabberwock.configuration import AXLClientSettings
-from jabberwock._version import __version__
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 from pathlib import Path
-from jabberwock._version import __version__
+from _version import __version__
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
